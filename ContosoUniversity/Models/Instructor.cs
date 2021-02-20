a1f8059952c8a6ee3acab730b8cb085e747e7004
Chao-Ting Wen <32146339+islysky18@users.noreply.github.com>
2021-02-20 16:07:11 -0500

    Creating a More Complex Data Model

---
+++ ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Last Name"), StringLength(50, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [Column("FirstName"), Display(Name = "First Name"), StringLength(50, MinimumLength = 1)]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date), Display(Name = "Hire Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+
+        //if a navigation property can hold multiple entities, its type must implement the ICollection<T> Interface. 
+        //For example IList<T> qualifies but not IEnumerable<T> because IEnumerable<T> doesn't implement Add.
+        public virtual ICollection<Course> Courses { get; set; }
+        public virtual OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
